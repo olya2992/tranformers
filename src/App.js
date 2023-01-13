408b7a021bbc0fe6e7b792cbb9fb3fb66652b47b
Olha Hanzenko <olhahanzenko@Denyss-MacBook-Air.local>
2023-01-13 13:50:50 -0600

    transformer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [robot, setRobot] = useState(0);
+const {id, name, description, image, about}=data[robot]
+const [bot, setBot] = useState(data)
+
+const removeItem =(id) => {
+  let newBots = bot.filter(bot => bot.id !== id)
+  setBot(newBots)
+}
+
+const previousRobot = () => {
+setRobot((robot =>{
+  robot --; 
+  if(robot<0){
+    return data.length - 1;
+  }
+  return robot;
+}))
+}
+ const nextRobot = () => {
+setRobot((robot =>{
+  robot ++;
+  if(robot > data.length -1){
+    robot = 0;
+
+  }
+  return robot;
+}))
+ }
+
+  return(
+    <div>
+    <div className='heading'>
+       <h1>TRANSFORMERS CHARACTERS</h1>
+         </div>
+
+         <div className='heading'>
+       <h3>Learn about the Transformers robots</h3>
+       </div>
+
+       <div className='container'>
+      <img src={image} width='400px' alt='robot' />
+       </div>
+
+       <div className='container'>
+        <h2>{name}</h2>
+       </div>
+       <div className='container'>
+        <h3 className='type'>{description}</h3>
+       </div>
+   
+   <div className='btn'>
+    <button className='array' onClick={previousRobot} > ← </button>
+    <button className='array' onClick={nextRobot} > →</button>
+</div>
+
+<div className='heading below'>
+       <h2>{bot.length} FAVORITE TRANSFORMERS</h2>
+       </div>
+ 
+{bot.map((element =>{
+  const {name, image} = element;
+  return(<div  >
+      <div className='bots'>
+        <h3>{name}</h3>
+      </div>
+      <div className='bots'>
+        <h4>{about}</h4>
+      </div>
+      
+
+
+      <div className='bots'>
+        <img  src={image} alt= 'bots' width='200px'/>
+      </div>
+
+      <div className='bots'>
+       <button onClick={()=>removeItem(id)}>DELETE</button>
+      </div>
+
     </div>
-  );
+  )
+}))}
+
+<div className='container'>
+<button className='delete' onClick={()=>setBot([])} >DELETE All</button>
+</div>
+       </div>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const data=[
+    { 
+        id: 1,
+        name: 'OPTIMUS-PRIME',
+        description: 'AUTOBOT',
+        image: 'https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/2b13f3777a3e4e496af61ca3b63d317b.png',
+        about: 'Optimus Prime is the noble leader of the heroic Autobots. He believes freedom is the right of all sentient beings. He carries the Matrix of Leadership, an ancient and powerful artifact that gives him upgraded abilities and knowledge of all Cybertronian history. He is beloved for his compassionate nature, steadfast leadership, and heroic feats in battle.'
+    },
+    {
+        id: 2,
+        name: 'BUMBLEBEE',
+        description: 'AUTOBOT',
+        image: 'https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/1212ba0351869e8a2df09cf059d54734.png',
+        about: 'When the Decepticons are on your tail, Bumblebee is the bot you want by your side. Smaller in size, he is often underestimated, but the Autobot scout is a warrior with a fearless spark who is always ready to defend his friends. Bumblebee has proven his worth in battle many times over and become one of the most trusted lieutenants of Optimus Prime.'
+    },
+    {
+        id: 3,
+        name: 'WINDBLADE',
+        description: 'AUTOBOT',
+        image: 'https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/0e234e023cddb05be4b4e8c7dabf3ab4.png',
+        about: 'Windblade is a Cityspeaker – a bot who can communicate telepathically with Titans, bots the size of cities. She fights selflessly for what she believes in and always tries to use reason when everyone else has abandoned it. When that doesn’t work, she uses her turbine-hilted sword Stormfall to solve matters once and for all.'
+    },
+    {
+        id: 4,
+        name: 'GRIMLOCK',
+        description: 'AUTOBOT',
+        image: 'https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/73d0bed16e333009a0bb24bd88760e6a.png',
+        about: 'The strength of the Dinobot leader Grimlock is legendary. His prowess in battle is unrivaled. He respects strength beyond all else, but he doesn’t like bullies. It’s this that fuels his dedication to fight against those who would use their power for evil.'
+    },
+    {
+        id: 5,
+        name: 'MEGATRON',
+        description: 'DECEPTICON',
+        image: 'https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/7ed8d30a8706e8bbfa55710e0d11ad3b.png',
+        about: 'Megatron is the merciless leader of the evil Decepticons and one of the most feared and powerful warriors to ever emerge from Cybertron. He is driven by a never-ending need for conquest and desire to vanquish all who oppose him. The only thing he hungers for more than the conquest of the Autobots is the demise of his arch nemesis, Optimus Prime.'
+    },
+    {
+        id: 6,
+        name: 'STARSCREAM',
+        description: 'DECEPTICON',
+        image: 'https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/818eb987773a9b46d1220f149987f940.png',
+        about: 'Unsurpassed in ego or ambition, Starscream is the scheming second-in-command to Megatron. His all-consuming hunger for absolute power overcomes what little reason there is in him, driving him to adopt ever more vile plots in his quest to unseat Megatron as leader of the evil Decepticons.'
+    }
+
+]
